9badb1f059c90529d48864641c3066337172f896
Jobobby04 <jobobby04@users.noreply.github.com>
2020-10-06 22:38:06 -0400

    Made android stub library, has one stub

---
+++ lib/android-stub/src/main/java/android/net/Uri.kt
@@ -0,0 +1,3 @@
+package android.net
+
+typealias Uri = String
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,9 @@ project(":duktape-stub").projectDir = File("lib/duktape-stub")
 include(":lib-dataimage")
 project(":lib-dataimage").projectDir = File("lib/dataimage")
 
+include(":lib-android-stub")
+project(":lib-android-stub").projectDir = File("lib/android-stub")
+
 File(rootDir, "src").eachDir { dir ->
     dir.eachDir { subdir ->
         val name = ":${dir.name}-${subdir.name}"
